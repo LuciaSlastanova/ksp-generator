87b2037d76129a7717a1fdebeb864ded3190541b
LuciaSlastanova <151719882+LuciaSlastanova@users.noreply.github.com>
2026-09-01 15:28:33 +0200

    Create database.py

---
+++ database.py
@@ -0,0 +1,45 @@
+import os
+import streamlit as st
+from supabase import create_client, Client
+
+
+def get_supabase_client() -> Client:
+
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_KEY"]
+
+    return create_client(url, key)
+
+
+def create_project(name, status="Rozpracované"):
+
+    supabase = get_supabase_client()
+
+    data = {
+        "name": name,
+        "status": status
+    }
+
+    response = (
+        supabase
+        .table("projects")
+        .insert(data)
+        .execute()
+    )
+
+    return response
+
+
+def get_projects():
+
+    supabase = get_supabase_client()
+
+    response = (
+        supabase
+        .table("projects")
+        .select("*")
+        .order("created_at", desc=True)
+        .execute()
+    )
+
+    return response.data
