d454a3ee2d50eb6698222148961225aa117948de
LuciaSlastanova <151719882+LuciaSlastanova@users.noreply.github.com>
2026-09-02 08:45:54 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,10 +1,12 @@
-import os
 import streamlit as st
 from supabase import create_client, Client
+from uuid import uuid4
+
+
+BUCKET_NAME = "project-documents"
 
 
 def get_supabase_client() -> Client:
-
     url = st.secrets["SUPABASE_URL"]
     key = st.secrets["SUPABASE_KEY"]
 
@@ -12,7 +14,6 @@ def get_supabase_client() -> Client:
 
 
 def create_project(name, status="Rozpracované"):
-
     supabase = get_supabase_client()
 
     data = {
@@ -27,11 +28,13 @@ def create_project(name, status="Rozpracované"):
         .execute()
     )
 
-    return response
+    if response.data:
+        return response.data[0]
+
+    return None
 
 
 def get_projects():
-
     supabase = get_supabase_client()
 
     response = (
@@ -39,6 +42,74 @@ def get_projects():
         .table("projects")
         .select("*")
         .order("createds_at", desc=True)
+        .execute()
+    )
+
+    return response.data
+
+
+def upload_project_file(
+    project_id,
+    uploaded_file,
+    document_type
+):
+    supabase = get_supabase_client()
+
+    # Unikátny názov, aby sa súbory neprepisovali
+    unique_name = f"{uuid4()}_{uploaded_file.name}"
+
+    file_path = (
+        f"{project_id}/"
+        f"{document_type}/"
+        f"{unique_name}"
+    )
+
+    file_bytes = uploaded_file.getvalue()
+
+    content_type = (
+        uploaded_file.type
+        if uploaded_file.type
+        else "application/octet-stream"
+    )
+
+    # 1. uloženie samotného súboru do Storage
+    supabase.storage.from_(
+        BUCKET_NAME
+    ).upload(
+        file_path,
+        file_bytes,
+        {
+            "content-type": content_type
+        }
+    )
+
+    # 2. uloženie informácie o súbore do tabuľky documents
+    document_data = {
+        "project_id": project_id,
+        "document_type": document_type,
+        "file_name": uploaded_file.name,
+        "file_path": file_path
+    }
+
+    response = (
+        supabase
+        .table("documents")
+        .insert(document_data)
+        .execute()
+    )
+
+    return response.data
+
+
+def get_project_documents(project_id):
+    supabase = get_supabase_client()
+
+    response = (
+        supabase
+        .table("documents")
+        .select("*")
+        .eq("project_id", project_id)
+        .order("created_at", desc=False)
         .execute()
     )
 
